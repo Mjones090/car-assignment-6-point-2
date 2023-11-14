da79fcdece80a665706856148070da1820a25c71
Mjones090 <145160703+Mjones090@users.noreply.github.com>
2023-11-14 17:44:51 +0000

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,26 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    private float speed = 20.0f;
+    //private float speed = 20.0f;
+    [SerializeField] private float horsePower = 0;
     private float turnSpeed = 45.0f;
     private float horizontalInput;
-    private float forwardInput;
+    private float verticalInput;
+    private Rigidbody playerRb;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        verticalInput = Input.GetAxis("Vertical");
         // Move vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+       //playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput * horsePower)
         //rotate car
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
